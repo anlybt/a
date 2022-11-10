f43a0389c52e3e49693d4600361226a99f60e0ec
anlybt <101204064+anlybt@users.noreply.github.com>
2022-11-10 17:19:22 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,4 @@
-for(let c=0;++c<1001;)console.log((c%3?"":"Fizz")+(c%5?"":"Buzz")+(c%7?"":"Bar")||c)
+
+export default function greet(name) {
+  return for(let c=0;++c<1001;)console.log((c%3?"":"Fizz")+(c%5?"":"Buzz")+(c%7?"":"Bar")||c);
+}
